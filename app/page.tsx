1f010186cc82a97a4c055d6bab7b50f28bcf3c38
daudmwakolo <abdallahmwakolo@gmail.com>
2026-04-26 02:44:58 +0300

    podium

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@
 import Header from './components/Header';
 import HotThree from './components/HotThree';
 import Throne from './components/Throne';
+import Podium from './components/Podium';
 
 export default function Home() {
 
@@ -16,25 +17,29 @@ export default function Home() {
       {/* HEADER */}
       <Header />
 
-      {/* THRONE (MAIN AUTHORITY LAYER) */}
-      <Throne
-        song="Bongo Anthem"
-        artist="Davieh G ft Matitu"
-        votes="12.4K"
-        weeksInChart={12}
-        weeksAtNo1={5}
-        ytRank="#01"
-        spRank="#02"
-        bpRank="#01"
-        onVote={handleVote}
-      />
-
-      {/* CONTENT */}
-      <div className="flex-grow pt-10 pb-20">
-
-        {/* HOT THREE */}
+      {/* HOT THREE (DISCOVERY ZONE - FIRST IMPACT) */}
+      <div className="pt-6">
         <HotThree />
+      </div>
 
+      {/* THRONE (MAIN AUTHORITY CENTER) */}
+      <div className="mt-8">
+        <Throne
+          song="Bongo Anthem"
+          artist="Davieh G ft Matitu"
+          votes="12.4K"
+          weeksInChart={12}
+          weeksAtNo1={5}
+          ytRank="#01"
+          spRank="#02"
+          bpRank="#01"
+          onVote={handleVote}
+        />
+      </div>
+
+      {/* PODIUM (ELITE CONFIRMATION LAYER) */}
+      <div className="mt-8 pb-20">
+        <Podium onVote={handleVote} />
       </div>
 
     </main>
