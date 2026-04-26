6cc71c88c8214702296a4f0f093fa3b3919830fb
daudmwakolo <abdallahmwakolo@gmail.com>
2026-04-26 14:47:15 +0300

    next js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-module.exports = nextConfig;
+const nextConfig = {
+    typescript: {
+      // !! WARN !!
+      // This allows production builds to successfully complete even if
+      // your project has type errors.
+      ignoreBuildErrors: true,
+    },
+    eslint: {
+      // This allows production builds to successfully complete even if
+      // your project has ESLint errors.
+      ignoreSuccessiveBuilds: true,
+      ignoreDuringBuilds: true,
+    },
+  }
+  
+  module.exports = nextConfig
