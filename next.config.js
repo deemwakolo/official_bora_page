2fdf34290a6c9e4f472cbc7ba8024dc055de1ca5
daudmwakolo <abdallahmwakolo@gmail.com>
2026-04-27 00:01:36 +0300

    supabase save

--- next.config.js
+++ next.config.js
@@ -1,17 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    typescript: {
-      // !! WARN !!
-      // This allows production builds to successfully complete even if
-      // your project has type errors.
-      ignoreBuildErrors: true,
-    },
-    eslint: {
-      // This allows production builds to successfully complete even if
-      // your project has ESLint errors.
-      ignoreSuccessiveBuilds: true,
-      ignoreDuringBuilds: true,
-    },
-  }
-  
-  module.exports = nextConfig
+  typescript: {
+    // ⚠ allows build even with TS errors (temporary only)
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // ⚠ allows build even with lint errors
+    ignoreDuringBuilds: true,
+  },
+};
+
+module.exports = nextConfig;
